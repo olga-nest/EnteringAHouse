5d22f4e8b4605e294c7094fdc1e64ba993c5d262
olga-nest <olga.w.nesterova@gmail.com>
2017-10-11 21:29:56 -0700

    Initial Commit

---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by Olga on 10/11/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
